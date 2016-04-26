5f0bedd6a51c98dd0cc6921814ecc9a5d0c9d6e1
17dlongo <danieldominiclongo@gmail.com>
2016-04-26 11:18:04 -0700

    Update Number.py

--- Number.py
+++ Number.py
@@ -1,3 +1,9 @@
+users = {}
+with  open("Users.txt") as f:
+    for line in f:
+        (key, val) = line.split
+        users[str(key)] = val
+        
 import random
 number = random.randint(1,100)
 player1 = input('Name of Player 1')
@@ -20,3 +26,28 @@ def is_it(number):
         is_it(number)
 
 is_it(number)
+
+def create_account(users):
+    username = input('Enter Your Username')
+    password = input('Enter Your Password')
+    conferm = input(Please Conferm Your Password')
+    if conferm == password:
+        name = input('Name')
+        email = input('Email')
+        points = 0
+        times_played = 0
+        users[username] = [password, name, email, points, times_played]
+        stf = open("Users.txt")
+        stf = None
+        stf = stf.append(users)
+    if conferm != password;
+        print("Values do not match")
+        if input('Would you like to try again(Yes)' == yes or Yes:
+            create_account(users)
+        else:
+            break
+
+
+
+
+
